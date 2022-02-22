adfc1f23d11237ae34b039f88c01b1ac8198b15b
Medha <tyagi.25@gmail.com>
2022-02-22 23:32:27 +0000

    parameterize azs and subnets

--- network-config/main.tf
+++ network-config/main.tf
@@ -2,9 +2,9 @@ module "vpc" {
   source                 = "terraform-aws-modules/vpc/aws"
   name                   = "wordpress-first-vpc"
   cidr                   = "10.0.0.0/16"
-  azs                    = ["us-east-1a", "us-east-1b"]
-  private_subnets        = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets         = ["10.0.101.0/24", "10.0.102.0/24"]
+  azs                    = [data.aws_availability_zones.available.names[0], data.aws_availability_zones.available.names[1]]
+  private_subnets        = [cidrsubnet("10.0.0.0/16", 8, 1), cidrsubnet("10.0.0.0/16", 8, 2)]
+  public_subnets         = [cidrsubnet("10.0.0.0/16", 8, 101), cidrsubnet("10.0.0.0/16", 8, 102)]
   enable_nat_gateway     = true
   enable_vpn_gateway     = false
   one_nat_gateway_per_az = true
